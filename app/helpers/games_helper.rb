43b7bb8495ecb79c1afeb693f1817777a984505b
Skyler Tom <skylertom@gmail.com>
2016-02-05 03:38:12 -0500

    fixing up games index interaction

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -1,7 +1,7 @@
 module GamesHelper
   # determines whether the current games view is selected
   def games_range(time_range)
-    if params[:time] == time_range
+    if params[:time] == time_range || (params[:time].blank? && time_range == "current")
       'active'
     else
       ''
